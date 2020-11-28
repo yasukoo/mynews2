18f0919fd10dfc7f411c0ce36dabbc9b17289737
yasukocchi <yasukochi.sp@gmail.com>
2020-11-28 08:06:45 +0000

    PHP/Laravel 08 ControllerとRoutingの関係について理解しよう

---
+++ app/Http/Controllers/admin/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view ('admin.plofile.create');
+    }
+    
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
+
