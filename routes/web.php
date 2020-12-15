d2747a9552786a9fc8f3e7b22a49db1d8b26ec46
yasukocchi <yasukochi.sp@gmail.com>
2020-12-15 12:25:30 +0000

    PHP/Laravel 12 Task submission

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     //課題４
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     
 });
 
@@ -28,3 +28,15 @@ Route::group(['prefix' => 'admin'], function() {
 Route::get('XXX', 'AAA@bbb');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
