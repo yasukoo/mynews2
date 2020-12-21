19c57088133aa23ed10dcad1e3bff68ebb2ab91c
yasukocchi <yasukochi.sp@gmail.com>
2020-12-21 14:45:43 +0000

    Revert "PHP/Laravel 13 Task submission"
    This reverts commit 91d616b77273d774e86d2536581a7d8545a5c439.

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,9 +12,5 @@ class NewsController extends Controller
     {
         return view('admin.news.create');
     }
-    public function create(Request $request)
-    {
-      // admin/news/createにリダイレクトする
-      return redirect('admin/news/create');
-    }  
 }
+
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     //課題４
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::post('profile/create', 'Admin\ProfileController@create');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
-    Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    
 });
 
 
