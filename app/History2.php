29f8b9e705d258d22ebc363595113c7e4672426c
yasukocchi <yasukochi.sp@gmail.com>
2020-12-30 08:10:59 +0000

    PHP/Laravel 17 Task submission

--- app/History2.php
+++ app/History2.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class History2 extends Model
 {
     protected $guarded = array('id');
-
+    protected $table = 'histories2';
     public static $rules = array(
         'profile_id' => 'required',
         'edited_at' => 'required',
