8aa3534915efce5689b7603cbaad46e947c2f819
yasukocchi <yasukochi.sp@gmail.com>
2020-12-30 08:51:30 +0000

    question2

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     Route::get('news/delete', 'Admin\NewsController@delete');
+    Route::get('/', 'NewsController@index');
     //課題４
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
